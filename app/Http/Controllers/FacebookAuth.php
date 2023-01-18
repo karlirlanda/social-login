c5f799a6f6967c88d2927035a22e65b0cfcc9004
karlirlanda <karlirlanda@gmail.com>
2023-01-18 11:16:29 +0800

    initial upload

---
+++ app/Http/Controllers/FacebookAuth.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class FacebookAuth extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('facebook')->stateless()->redirect();
+    }
+
+    public function callbackFacebook()
+    {
+        $facebook_user = Socialite::driver('facebook')->stateless()->user();
+        $user = User::where('facebook_id', $facebook_user->id)->first();
+
+        if (!$user) {
+            $new_user = User::create([
+                'name' => $facebook_user->name,
+                'email' => $facebook_user->email,
+                'facebook_id' => $facebook_user->id,
+            ]);
+
+            Auth::login($new_user);
+
+            return redirect()->intended('dashboard');
+        } else {
+            Auth::login($user);
+
+            return redirect()->intended('dashboard');
+        }
+    }
+}
---
+++ app/Http/Controllers/GoogleAuth.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleAuth extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->stateless()->redirect();
+    }
+
+    public function callbackGoogle()
+    {
+        $google_user = Socialite::driver('google')->stateless()->user();
+        $user = User::where('google_id', $google_user->id)->first();
+
+        if(!$user){
+            $new_user = User::create([
+                'name' => $google_user->name,
+                'email' => $google_user->email,
+                'google_id' => $google_user->id,
+            ]);
+
+            Auth::login($new_user);
+
+            return redirect()->intended('dashboard');   
+        } else {
+            Auth::login($user);
+
+            return redirect()->intended('dashboard');
+        }
+    }
+}
